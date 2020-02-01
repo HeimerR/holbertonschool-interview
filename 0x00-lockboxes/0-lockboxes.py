1e699773fa12cd74b626541d0c8c5e9b91c3a297
HeimerR <ing.heimer.rojas@gmail.com>
2020-02-01 23:43:24 +0000

    endless while loop solution

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,7 +6,7 @@ def updateboxes(boxes, boxes_state, keys):
     """ updates the state of boxes """
     last_keys = []
     for i in range(len(keys)):
-        boxes_state[keys[i]] = True
+        boxes_state[keys[i]] = "open"
         last_keys += boxes[keys[i]]
     return last_keys
 
@@ -14,7 +14,11 @@ def updateboxes(boxes, boxes_state, keys):
 def canUnlockAll(boxes):
     """  a method that determines if all the boxes can be opened """
     keys = [0]
-    boxes_state = [False for i in boxes]
-    for i in range(len(boxes)):
+    boxes_state = ["close" for i in boxes]
+    while(True):
+        prev_boxes_state = [row[:] for row in boxes_state]
         keys = updateboxes(boxes, boxes_state, keys)
-    return all(boxes_state)
+        if prev_boxes_state == boxes_state:
+            return False
+        if (len(set(boxes_state)) == 1):
+            return True
