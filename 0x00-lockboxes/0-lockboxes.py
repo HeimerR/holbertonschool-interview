0de4f2e0160be6b77e2d2d0f41cf2d1d0f1901d7
HeimerR <ing.heimer.rojas@gmail.com>
2020-02-02 13:22:07 +0000

    out of range condition added

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,17 +4,19 @@
 
 def updateboxes(boxes, boxes_state, keys):
     """ updates the state of boxes """
-    last_keys = []
+    new_keys = []
     for i in range(len(keys)):
-        boxes_state[keys[i]] = "open"
-        last_keys += boxes[keys[i]]
-    return last_keys
+        if keys[i] < len(boxes):
+            boxes_state[keys[i]] = "open"
+            new_keys += boxes[keys[i]]
+    return new_keys
 
 
 def canUnlockAll(boxes):
     """  a method that determines if all the boxes can be opened """
-    keys = [0]
+    keys = boxes[0]
     boxes_state = ["close" for i in boxes]
+    boxes_state[0] = "open"
     while(True):
         prev_boxes_state = [row[:] for row in boxes_state]
         keys = updateboxes(boxes, boxes_state, keys)
