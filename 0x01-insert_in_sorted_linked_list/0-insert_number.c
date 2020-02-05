461a1b93380e7bc29d6c8fcc16b7ffc21b14712e
HeimerR <ing.heimer.rojas@gmail.com>
2020-02-05 16:00:13 +0000

    draft insert function

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,38 @@
+include "lists.h"
+/**
+*
+*
+*
+*
+*
+*
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *current;
+
+	current = *head;
+	new = malloc(sizeof(listint_t));
+	new->n = number;
+
+	if (new == NULL)
+        	return (NULL);
+	
+	if (current == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		while (current->next != NULL && current->next->n < number)
+			current = current->next;
+
+		new->next = current->next;
+		current->next = new;
+	}
+	
+	return (new)
+}
+
