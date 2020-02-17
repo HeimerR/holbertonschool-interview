a34fea89870812fb50b906ffdfc1094f305d5562
HeimerR <ing.heimer.rojas@gmail.com>
2020-02-17 13:50:12 +0000

    betty stlye

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,6 +1,6 @@
 #ifndef HEAP_H
 #define HEAP_H
-#include <stddef.h>
+#include <stdlib.h>
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -11,10 +11,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s heap_t, binary_tree_t;
 void binary_tree_print(const binary_tree_t *);
