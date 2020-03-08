58231821907e15c0454319c57a4b5b299b9263e0
HeimerR <ing.heimer.rojas@gmail.com>
2020-03-08 16:51:17 +0000

    array version

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -2,47 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * add_nodeint_head - adds a new node at the head of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint_head(listint_t *head, const int n)
-{
-	listint_t *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = head;
-	head = new;
-
-	return (new);
-}
-/**
- * clone - clone a list
- * @head: pointer to pointer of first node of listint_t list
- * Return: address of the clone head
- */
-listint_t *clone(listint_t **head)
-{
-	listint_t *new;
-	listint_t *current;
-
-	current = *head;
-	new = NULL;
-
-	while (current != NULL)
-	{
-		new = add_nodeint_head(new, current->n);
-		current = current->next;
-	}
-
-	return (new);
-}
-/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: head of the list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
@@ -51,26 +10,22 @@ listint_t *clone(listint_t **head)
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *new;
-	listint_t *tmp;
 	listint_t *current;
+	int i = 0, j;
+	int array[5000];
 
 	current = *head;
 	if (current == NULL)
 		return (1);
-	new = clone(head);
-	tmp = new;
 	while (current != NULL)
 	{
-		if (new->n != current->n)
-		{
-			free_listint(tmp);
-			return (0);
-		}
+		array[i++] = current->n;
 		current = current->next;
-		new = new->next;
 	}
-	free_listint(tmp);
+	for (j = 0; j <= (i / 2) + 1; j++)
+	{
+		if (array[j] != array[--i])
+			return (0);
+	}
 	return (1);
 }
-
