ffec8b853b56aee559abcd428088fe051fe657bd
HeimerR <ing.heimer.rojas@gmail.com>
2020-03-29 16:53:08 +0000

    palidrome detector added

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "palindrome.h"
+/**
+ * is_palindrome - t checks whether or not a given
+ * integer is a palindrome
+ * @n: integer
+ * Return: 1 if n is a palindrome, and 0 otherwise
+ */
+int is_palindrome(unsigned long n)
+{
+	unsigned long i = 1, j = 10, n2 = n;
+
+	if (n < 10)
+		return (1);
+
+	while (i < n)
+		i *= 10;
+
+	i /= 10;
+
+	while (i > 1)
+	{
+		if (n2 % j != n2 / i)
+			return (0);
+		n2 = (n2 % i) / 10;
+		i /= 100;
+	}
+	return (1);
+}
