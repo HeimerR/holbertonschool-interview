62dc1fc24de6660e14739edeaea513a1bba9bdf8
HeimerR <ing.heimer.rojas@gmail.com>
2020-04-03 16:56:41 +0000

    utf validator added

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" UTF-8 Validation """
+
+
+def validUTF8(data):
+    """ determines if a given data set
+        represents a valid UTF-8 encoding
+    """
+    d = [n & 255 for n in data]
+    labels = [False for n in d]
+    i = 0
+    while i < len(d):
+        if d[i] < 128:
+            labels[i] = True
+            i += 1
+        elif d[i] & 240 == 224:
+            if (i+3 < len(d)-1):
+                labels[i] = True
+                i += 1
+                for n in range(3):
+                    if d[i+n] & 192 == 128:
+                        labels[i+n] = True
+                    else:
+                        break
+                i += 2
+            else:
+                break
+        elif d[i] & 224 == 192:
+            if (i+2 < len(d)-1):
+                labels[i] = True
+                i += 1
+                for n in range(2):
+                    if d[i+n] & 192 == 128:
+                        labels[i+n] = True
+                    else:
+                        break
+                i += 1
+            else:
+                break
+        else:
+            break
+    return all(labels)
