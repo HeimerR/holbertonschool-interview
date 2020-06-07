21e0600fdeb4631fd3a1f46d2eecc1268524a13e
HeimerR <ing.heimer.rojas@gmail.com>
2020-06-07 21:16:06 +0000

    print deleted

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -14,12 +14,9 @@ def rain(walls):
     water = 0
     while l1 >= 3:
         w2 = walls.copy()
-        # print("w2", w2)
         w2.sort()
         top = w2[-1]
-        # print("top", top)
         top2 = w2[-2]
-        # print("top2", top2)
         idx1 = walls.index(top)
         idx2 = walls.index(top2)
         if idx1 == idx2:
@@ -27,11 +24,8 @@ def rain(walls):
         idx = [idx1, idx2]
         idx.sort()
         limit = min([walls[idx[0]], walls[idx[1]]])
-        # print("idx", idx)
         for i in range(idx[0]+1, idx[1]):
-            # print("i", i)
             water += limit - walls[i]
-            # print("water:", water)
         for i in range(idx[0], idx[1]+1):
             del walls[idx[0]]
         walls.insert(idx[0], limit)
