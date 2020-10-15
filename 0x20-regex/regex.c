ea0167981c902b8d6f76ef78a839d7078dd62b6d
HeimerR <ing.heimer.rojas@gmail.com>
2020-10-15 17:31:50 +0000

    rollback bash ver

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -1,32 +1,4 @@
 #include "regex.h"
-#include <stdlib.h>
-/**
- * regex_batch - Checks whether a given pattern matches a given string
- * @str: is the string to scan
- * @pattern: is the regular expression
- * Return: 1 if the pattern matches the string, or 0 if it doesn’t
- */
-int regex_batch(char const *str, char const *pattern)
-{
-	int status;
-	char *bash;
-	int l1, l2;
-
-	l1 = strlen(str);
-	l2 = strlen(pattern);
-	bash = malloc(sizeof(char *) * (8 + l1 + l2));
-	strcat(bash, "./regex_bash ");
-	strcat(bash, str);
-	strcat(bash, " ");
-	strcat(bash, pattern);
-
-	/*printf("%s\n", bash);*/
-	status = system(bash);
-	if (status != 0)
-		return (1);
-	return (status);
-}
-
 /**
  * regex_match - Checks whether a given pattern matches a given string
  * @str: is the string to scan
@@ -54,9 +26,22 @@ int regex_match(char const *str, char const *pattern)
 		}
 		return (1);
 	}
-	if (pattern[0] == '.' && pattern[1] == '*' && pattern[2] == '\n')
+	if (pattern[0] == '.' && pattern[1] == '*' && pattern[2] == '\0')
 		return (1);
 	if (!strchr(pattern, '.') && strchr(pattern, '*'))
-		return (regex_batch(str, pattern));
+	{
+		if (pattern[1] == '*' && pattern[2] == '\0')
+		{
+			while (str[j])
+			{
+				if (str[j] == pattern[0])
+					j++;
+				else
+					return (0);
+			}
+			return (1);
+
+		}
+	}
 	return (1);
 }
