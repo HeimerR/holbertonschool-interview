1c01e52fd7bd7f1248de490fa09c3a28b0c342b8
HeimerR <ing.heimer.rojas@gmail.com>
2020-10-18 18:12:53 +0000

    free blocks added

--- 0x21-substring/substring.c
+++ 0x21-substring/substring.c
@@ -4,7 +4,7 @@
  * @a: first value
  * @b: second value
  * Return: substraction
-**/
+ **/
 int cmpfunc(const void *a, const void *b)
 {
 	return (*(int *)a - *(int *)b);
@@ -15,7 +15,7 @@ int cmpfunc(const void *a, const void *b)
  * @wordlen: word size
  * @idx_len: aux_indx size
  * Return: index firt word or zero
-**/
+ **/
 int check(int *aux_indx, int wordlen, int idx_len)
 {
 	int i;
@@ -33,7 +33,7 @@ int check(int *aux_indx, int wordlen, int idx_len)
  * @words: array of words to searh in the string
  * @nb_words: number of words
  * Return: array with index for every word or NULL
-**/
+ **/
 int *search(char const *s, char const **words, int nb_words)
 {
 	int *aux_indx, i, j;
@@ -45,7 +45,10 @@ int *search(char const *s, char const **words, int nb_words)
 	{
 		aux = strstr(s, words[i]);
 		if (!aux)
+		{
+			free(aux_indx);
 			return (NULL);
+		}
 		len = (int)strlen(aux);
 		/*printf("holas\n");*/
 		/*printf("sublen: %d\n", len);*/
@@ -56,7 +59,10 @@ int *search(char const *s, char const **words, int nb_words)
 			{
 				aux = strstr(s + strlen(s) - len + 1, words[i]);
 				if (!aux)
+				{
+					free(aux_indx);
 					return (NULL);
+				}
 				len = (int)strlen(aux);
 				aux_indx[i] = len;
 				continue;
@@ -80,7 +86,7 @@ int *search(char const *s, char const **words, int nb_words)
  **/
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-	int *indx, *aux_indx, wordlen, j, f_indx, diff, s_len;
+	int *indx, *aux_indx, wordlen, j, f_indx, diff, s_len, *indx_final;
 
 	indx = malloc((sizeof(int)) * strlen(s));
 	*n = 0;
@@ -96,6 +102,7 @@ int *find_substring(char const *s, char const **words, int nb_words, int *n)
 			break;
 		qsort(aux_indx, nb_words, sizeof(int), cmpfunc);
 		f_indx = check(aux_indx, wordlen, nb_words);
+		free(aux_indx);
 		if (f_indx)
 		{
 			diff = strlen(s) - f_indx + j;
@@ -108,6 +115,9 @@ int *find_substring(char const *s, char const **words, int nb_words, int *n)
 		}
 		s++;
 	}
-	*n = 0;
-	return (NULL);
+	indx_final = malloc((sizeof(int)) * (*n));
+	for (j = 0; j < *n; j++)
+		indx_final[j] = indx[j];
+	free(indx);
+	return (indx_final);
 }
